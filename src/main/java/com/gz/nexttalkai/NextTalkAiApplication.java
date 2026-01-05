79ea12b7ae126b0ef9b36ab6797afbfd6e5b1fb3
guozhong <17853522814@163.com>
2026-01-05 21:47:29 +0800

    commit:project init

---
+++ src/main/java/com/gz/nexttalkai/NextTalkAiApplication.java
@@ -0,0 +1,13 @@
+package com.gz.nexttalkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTalkAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextTalkAiApplication.class, args);
+    }
+
+}
